76257a7fea7b074bce1d6d8e057612e7521e5428
ImRishabhRawat <rishabhrawat757@gmail.com>
2024-05-24 10:15:39 +0530

    updated code

--- server/index.js
+++ server/index.js
@@ -4,15 +4,15 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import passport from "passport";
-import roomRoute from './routes/room.js'
-import authRoute from './routes/auth.js'
-import googleRoute from "./routes/googleAuth.js";
-import './passport.js';  
-import userRoute from "./routes/user.js";
-import bookingRoute from "./routes/booking.js"
-import githubRoute from "./routes/githubAuth.js"
 import session from "express-session";
+import roomRoute from "./routes/room.js";
+import authRoute from "./routes/auth.js";
+import googleRoute from "./routes/googleAuth.js";
+import userRoute from "./routes/user.js";
+import bookingRoute from "./routes/booking.js";
+import githubRoute from "./routes/githubAuth.js";
 import ownerRoutes from "./routes/owner.js";
+import "./passport.js";
 
 dotenv.config();
 
@@ -30,44 +30,44 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 const port = process.env.PORT || 8000;
 
 const corsOptions = {
-    origin: true,
-    Credentials: true,
+	origin: process.env.CLIENT_SITE_URL || true,
+	credentials: true,
 };
 
 app.get("/", (req, res) => {
 	res.send("API is working");
 });
 
-//database Connection
-mongoose.set('strictQuery', false);
+// Database Connection
+mongoose.set("strictQuery", false);
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URL)
-        console.log("MongoDB connection established");
-    } catch (error) { 
-        console.log(error);
-    }
-}
+	try {
+		await mongoose.connect(process.env.MONGODB_URL, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+		console.log("MongoDB connection established");
+	} catch (error) {
+		console.log(error);
+	}
+};
 
-//middleware
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/room", roomRoute);
-app.use("/api/v1/github",githubRoute)
+app.use("/api/v1/github", githubRoute);
 app.use("/api/v1/Oauth", googleRoute);
 app.use("/api/v1/user", userRoute);
-app.use("/api/v1/booking", bookingRoute)
+app.use("/api/v1/booking", bookingRoute);
 app.use("/api/v1/owners", ownerRoutes);
 
-
-
-app.listen(port, ()=>{
-    connectDB();
-    console.log("Server is listening on port " + port);
+app.listen(port, () => {
+	connectDB();
+	console.log(`Server is listening on port ${port}`);
 });
